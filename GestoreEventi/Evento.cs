779448083595da9bc36d83b504906561a20e41ac
Lorenzo <lorenzo.miccio97@gmail.com>
2022-04-22 10:21:14 +0200

    Aggionto costruttore, getter e setter

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class Evento
+    {
+        private string titolo;
+        private DateTime dataEvento;
+        private int capienzaMassimaEvento;
+        private int numeroPostiPrenotati;
+
+        public Evento(string titolo, DateTime dataEvento, int capienzaMassimaEvento)
+        {
+            this.titolo = titolo;
+            this.capienzaMassimaEvento = capienzaMassimaEvento;
+            this.numeroPostiPrenotati = 0;
+            if (dataEvento < DateTime.Now)
+            {
+                throw new InvalidDataException("La data dell'appuntamento non può essere nel passato");
+            }
+            else
+            {
+
+                this.dataEvento = dataEvento;
+            } 
+        }
+
+        //Getter
+        public string GetTitolo()
+        {
+            return titolo;
+        }
+        public DateTime GetDataEvento()
+        {
+            return dataEvento;
+        }
+        public int GetCapienzaMassimaEvento()
+        {
+            return capienzaMassimaEvento;
+        }
+        public int GetnumeroPostiPrenotati()
+        {
+            return numeroPostiPrenotati;
+        }
+
+        //Setter
+        public void SetTitolo(string nuovoTitolo)
+        {
+            titolo = nuovoTitolo;
+        }
+        public void SetData(DateTime nuovaData)
+        {
+            if (nuovaData < DateTime.Now)
+            {
+                throw new InvalidDataException("La data dell'appuntamento non può essere nel passato");
+            }
+            else
+            {
+
+                this.dataEvento = nuovaData;
+            }
+        }
+    }
+}
